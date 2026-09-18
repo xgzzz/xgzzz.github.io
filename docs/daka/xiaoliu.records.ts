f23e8424a608728220e00d4c5aef26ccbaaa59b9
zhangxg-c <zhangxg-c@glodon.com>
2026-09-18 09:43:32 +0800

    sync(daka): xiaoliu 2026-09-16 至 09-17 记录

--- docs/daka/xiaoliu.records.ts
+++ docs/daka/xiaoliu.records.ts
@@ -24,5 +24,7 @@ export const range = {
 
 export const records: DakaRecord[] = [
   { date: '2026-09-14' },
-  { date: '2026-09-15' }
+  { date: '2026-09-15' },
+  { date: '2026-09-16' },
+  { date: '2026-09-17' }
 ]
